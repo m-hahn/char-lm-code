16cde620992c1ffdf07cbb19ab2f5b9700daf2ad
m-hahn <mhahn29@gmail.com>
2018-07-04 11:15:07 -0600

    probing lm

---
+++ char-lm-ud-stationary-vocab-probe.py
@@ -0,0 +1,185 @@
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--language", dest="language", type=str)
+parser.add_argument("--load-from", dest="load_from", type=str)
+#parser.add_argument("--save-to", dest="save_to", type=str)
+
+import random
+
+parser.add_argument("--batchSize", type=int, default=16)
+parser.add_argument("--char_embedding_size", type=int, default=100)
+parser.add_argument("--hidden_dim", type=int, default=1024)
+parser.add_argument("--layer_num", type=int, default=1)
+parser.add_argument("--weight_dropout_in", type=float, default=0.01)
+parser.add_argument("--weight_dropout_hidden", type=float, default=0.1)
+parser.add_argument("--char_dropout_prob", type=float, default=0.33)
+parser.add_argument("--char_noise_prob", type = float, default= 0.01)
+parser.add_argument("--learning_rate", type = float, default= 0.1)
+parser.add_argument("--myID", type=int, default=random.randint(0,1000000000))
+parser.add_argument("--sequence_length", type=int, default=50)
+
+
+args=parser.parse_args()
+print(args)
+
+
+
+
+
+from corpusIterator import CorpusIterator
+training = CorpusIterator(args.language, partition="train", storeMorph=True, removePunctuation=True)
+dev = CorpusIterator(args.language, partition="dev", storeMorph=True, removePunctuation=True)
+
+def plus(it1, it2):
+   for x in it1:
+      yield x
+   for x in it2:
+      yield x
+
+try:
+   with open("/checkpoint/mhahn/char-vocab-"+args.language, "r") as inFile:
+     itos = inFile.read().strip().split("\n")
+except FileNotFoundError:
+    print("Creating new vocab")
+    char_counts = {}
+    # get symbol vocabulary
+    for sentence in plus(training.iterator(), dev.iterator()):
+       for line in sentence:
+           for char in line["word"]:
+              char_counts[char] = char_counts.get(char, 0) + 1
+    char_counts = [(x,y) for x, y in char_counts.items()]
+    itos = [x for x,y in sorted(char_counts, key=lambda z:(z[0],-z[1])) if y > 50]
+    with open("/checkpoint/mhahn/char-vocab-"+args.language, "w") as outFile:
+       print("\n".join(itos), file=outFile)
+#itos = sorted(itos)
+print(itos)
+stoi = dict([(itos[i],i) for i in range(len(itos))])
+
+
+
+
+import random
+
+
+import torch
+
+print(torch.__version__)
+
+from weight_drop import WeightDrop
+
+
+rnn = torch.nn.LSTM(args.char_embedding_size, args.hidden_dim, args.layer_num).cuda()
+
+rnn_parameter_names = [name for name, _ in rnn.named_parameters()]
+print(rnn_parameter_names)
+#quit()
+
+
+rnn_drop = WeightDrop(rnn, [(name, args.weight_dropout_in) for name, _ in rnn.named_parameters() if name.startswith("weight_ih_")] + [ (name, args.weight_dropout_hidden) for name, _ in rnn.named_parameters() if name.startswith("weight_hh_")])
+
+output = torch.nn.Linear(args.hidden_dim, len(itos)+3).cuda()
+
+char_embeddings = torch.nn.Embedding(num_embeddings=len(itos)+3, embedding_dim=args.char_embedding_size).cuda()
+
+logsoftmax = torch.nn.LogSoftmax(dim=2)
+
+train_loss = torch.nn.NLLLoss(ignore_index=0)
+print_loss = torch.nn.NLLLoss(size_average=False, reduce=False, ignore_index=0)
+char_dropout = torch.nn.Dropout2d(p=args.char_dropout_prob)
+
+modules = [rnn, output, char_embeddings]
+def parameters():
+   for module in modules:
+       for param in module.parameters():
+            yield param
+
+optim = torch.optim.SGD(parameters(), lr=args.learning_rate, momentum=0.0) # 0.02, 0.9
+
+named_modules = {"rnn" : rnn, "output" : output, "char_embeddings" : char_embeddings, "optim" : optim}
+
+if args.load_from is not None:
+  checkpoint = torch.load("/checkpoint/mhahn/"+args.load_from+".pth.tar")
+  for name, module in named_modules.items():
+      print(name)
+      module.load_state_dict(checkpoint[name])
+
+from torch.autograd import Variable
+
+
+# ([0] + [stoi[training_data[x]]+1 for x in range(b, b+sequence_length) if x < len(training_data)]) 
+
+#from embed_regularize import embedded_dropout
+
+def encodeWord(word):
+      numeric = [[0]]
+      for char in word:
+                numeric[-1].append((stoi[char]+3 if char in stoi else 2) if True else 2+random.randint(0, len(itos)))
+      return numeric
+
+rnn_drop.train(False)
+
+def encodeSequence(numeric):
+      input_tensor = Variable(torch.LongTensor(numeric).transpose(0,1).cuda(), requires_grad=False)
+      embedded = char_embeddings(input_tensor)
+      out, hidden = rnn_drop(embedded, None)
+      return out[-1].view(-1), hidden[0].view(-1), hidden[1].view(-1)
+
+
+import numpy as np
+
+def predictNext(numeric):
+     out, hidden, cell = encodeSequence(numeric)
+     prediction = logsoftmax(output(out.view(1,1,-1))).view(3+len(itos)).data.cpu().numpy()
+     predicted = np.argmax(prediction)
+     return itos[predicted-3] #, prediction
+
+out1, hidden1, cell1 = encodeSequence(encodeWord("katze"))
+out2, hidden2, cell2 = encodeSequence(encodeWord("katzem"))
+#print(torch.dot(out1[-1], out2[-1]))
+#print(torch.dot(hidden1[0], hidden2[0]))
+#print(torch.dot(hidden1[1], hidden2[1]))
+
+print(torch.nn.functional.cosine_similarity(out1, out2, dim=0))
+print(torch.nn.functional.cosine_similarity(hidden1, hidden2, dim=0))
+print(torch.nn.functional.cosine_similarity(cell1, cell2, dim=0))
+
+print(predictNext(encodeWord("willmach")))
+print(predictNext(encodeWord("habegemach")))
+print(predictNext(encodeWord("geseh")))
+print(predictNext(encodeWord("gedach")))
+print(predictNext(encodeWord("habegepups")))
+print(predictNext(encodeWord("habegegurk")))
+print(predictNext(encodeWord("habegerief")))
+print(predictNext(encodeWord("ichwerdepups")))
+
+print(predictNext(encodeWord("einekatze")))
+print(predictNext(encodeWord("zweikatze")))
+print(predictNext(encodeWord("derkater")))
+print(predictNext(encodeWord("deskater")))
+
+
+plurals = set()
+
+genders = dict([("Gender="+x, set()) for x in ["Masc", "Fem", "Neut"]])
+
+for sentence in training.iterator():
+    for line in sentence:
+     if line["posUni"] == "NOUN":
+      morph = line["morph"]
+      if "Number=Sing" in morph:
+        gender = [x for x in morph if x.startswith("Gender=")]
+        if len(gender) > 0:
+          genders[gender[0]].add(line["word"].lower())
+
+      if "Number=Plur" in  morph and "Case=Dat" not in morph:
+        if "|" not in line["lemma"] and line["lemma"].lower() != line["word"]:
+          plurals.add((line["lemma"].lower(), line["word"]))
+print(plurals)
+
+
+print(genders)
+
+
+
+
---
+++ char-lm-ud-stationary.py
@@ -0,0 +1,248 @@
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--language", dest="language", type=str)
+parser.add_argument("--load-from", dest="load_from", type=str)
+parser.add_argument("--save-to", dest="save_to", type=str)
+
+import random
+
+parser.add_argument("--batchSize", type=int, default=16)
+parser.add_argument("--char_embedding_size", type=int, default=100)
+parser.add_argument("--hidden_dim", type=int, default=1024)
+parser.add_argument("--layer_num", type=int, default=1)
+parser.add_argument("--weight_dropout_in", type=float, default=0.01)
+parser.add_argument("--weight_dropout_hidden", type=float, default=0.1)
+parser.add_argument("--char_dropout_prob", type=float, default=0.33)
+parser.add_argument("--char_noise_prob", type = float, default= 0.01)
+parser.add_argument("--learning_rate", type = float, default= 0.1)
+parser.add_argument("--myID", type=int, default=random.randint(0,1000000000))
+parser.add_argument("--sequence_length", type=int, default=50)
+
+
+args=parser.parse_args()
+print(args)
+
+
+
+
+
+from corpusIterator import CorpusIterator
+training = CorpusIterator(args.language, partition="train", storeMorph=False, removePunctuation=True)
+dev = CorpusIterator(args.language, partition="dev", storeMorph=False, removePunctuation=True)
+
+def plus(it1, it2):
+   for x in it1:
+      yield x
+   for x in it2:
+      yield x
+
+itos = set()
+# get symbol vocabulary
+for sentence in plus(training.iterator(), dev.iterator()):
+   for line in sentence:
+       for char in line["word"]:
+          itos.add(char)
+
+itos = list(itos)
+itos = sorted(itos)
+print(itos)
+stoi = dict([(itos[i],i) for i in range(len(itos))])
+
+
+
+
+import random
+
+
+import torch
+
+print(torch.__version__)
+
+from weight_drop import WeightDrop
+
+
+rnn = torch.nn.LSTM(args.char_embedding_size, args.hidden_dim, args.layer_num).cuda()
+
+rnn_parameter_names = [name for name, _ in rnn.named_parameters()]
+print(rnn_parameter_names)
+#quit()
+
+
+rnn_drop = WeightDrop(rnn, [(name, args.weight_dropout_in) for name, _ in rnn.named_parameters() if name.startswith("weight_ih_")] + [ (name, args.weight_dropout_hidden) for name, _ in rnn.named_parameters() if name.startswith("weight_hh_")])
+
+output = torch.nn.Linear(args.hidden_dim, len(itos)+2).cuda()
+
+char_embeddings = torch.nn.Embedding(num_embeddings=len(itos)+2, embedding_dim=args.char_embedding_size).cuda()
+
+logsoftmax = torch.nn.LogSoftmax(dim=2)
+
+train_loss = torch.nn.NLLLoss(ignore_index=0)
+print_loss = torch.nn.NLLLoss(size_average=False, reduce=False, ignore_index=0)
+char_dropout = torch.nn.Dropout2d(p=args.char_dropout_prob)
+
+modules = [rnn, output, char_embeddings]
+def parameters():
+   for module in modules:
+       for param in module.parameters():
+            yield param
+
+optim = torch.optim.SGD(parameters(), lr=args.learning_rate, momentum=0.0) # 0.02, 0.9
+
+named_modules = {"rnn" : rnn, "output" : output, "char_embeddings" : char_embeddings, "optim" : optim}
+
+if args.load_from is not None:
+  checkpoint = torch.load("/checkpoint/mhahn/"+args.load_from+".pth.tar")
+  for name, module in named_modules.items():
+      module.load_state_dict(checkpoint[name])
+
+from torch.autograd import Variable
+
+
+# ([0] + [stoi[training_data[x]]+1 for x in range(b, b+sequence_length) if x < len(training_data)]) 
+
+#from embed_regularize import embedded_dropout
+
+
+def encodeUtterance(utterance, train=True):
+     numeric = [0]
+     boundaries = []
+     for word in utterance:
+        for char in word["word"]:
+          numeric.append(stoi[char]+2 if random.random() > args.char_noise_prob else 2+random.randint(0, len(itos)-1))
+        boundaries.append(len(numeric))
+     numeric.append(1)
+     return boundaries, numeric
+
+
+def prepareDataset(data):
+      data = list(data)
+      random.shuffle(data)
+      boundaries = [[]]
+      numeric = [[0]]
+      for sentence in data:
+         for word in sentence:
+             for char in word["word"]:
+                numeric[-1].append(stoi[char]+2 if random.random() > args.char_noise_prob else 2+random.randint(0, len(itos)-1))
+                if len(numeric[-1]) > args.sequence_length:
+                   numeric.append([])
+                   boundaries.append([])
+             boundaries[-1].append(len(numeric[-1]))
+         numeric[-1].append(1)
+         if len(numeric[-1]) > args.sequence_length:
+            numeric.append([])
+            boundaries.append([])
+         boundaries[-1].append(len(numeric[-1]))
+      numeric[-1] = numeric[-1] + ([0]*(args.sequence_length + 1 - len(numeric[-1])))
+      result= list(zip(numeric, boundaries))
+      random.shuffle(result)
+      return result
+
+def forward(numericAndBoundaries, train=True, printHere=False):
+     # print(numeric)
+      numeric, boundaries = zip(*numericAndBoundaries)
+      input_tensor = Variable(torch.LongTensor(numeric).transpose(0,1)[:-1].cuda(), requires_grad=False)
+      target_tensor = Variable(torch.LongTensor(numeric).transpose(0,1)[1:].cuda(), requires_grad=False)
+
+
+    #  print(char_embeddings)
+      #if train and (embedding_full_dropout_prob is not None):
+      #   embedded = embedded_dropout(char_embeddings, input_tensor, dropout=embedding_full_dropout_prob, scale=None) #char_embeddings(input_tensor)
+      #else:
+      embedded = char_embeddings(input_tensor)
+      if train:
+         embedded = char_dropout(embedded)
+
+      out, _ = rnn_drop(embedded, None)
+#      if train:
+#          out = dropout(out)
+
+      logits = output(out) 
+      log_probs = logsoftmax(logits)
+   #   print(logits)
+  #    print(log_probs)
+ #     print(target_tensor)
+
+      loss = train_loss(log_probs.view(-1, len(itos)+2), target_tensor.view(-1))
+
+      if printHere:
+         lossTensor = print_loss(log_probs.view(-1, len(itos)+2), target_tensor.view(-1)).view(args.sequence_length, len(numeric))
+         losses = lossTensor.data.cpu().numpy()
+         boundaries_index = [0 for _ in numeric]
+         for i in range((args.sequence_length-1)-1):
+            if boundaries_index[0] < len(boundaries[0]) and i+1 == boundaries[0][boundaries_index[0]]:
+               boundary = True
+               boundaries_index[0] += 1
+            else:
+               boundary = False
+            print((losses[i][0], itos[numeric[0][i+1]-2], boundary))
+      return loss, len(numeric) * args.sequence_length
+
+def backward(loss, printHere):
+      optim.zero_grad()
+      if printHere:
+         print(loss)
+      loss.backward()
+      optim.step()
+
+
+
+
+import time
+
+devLosses = []
+for epoch in range(10000):
+   print(epoch)
+   training_data = training.iterator()
+   numericAndBoundaries = prepareDataset(training_data)
+
+
+
+
+#   oldParameters = {}
+#   for name, param in rnn.named_parameters():
+#      oldParameters[name] = param
+#      setattr(rnn, name, torch.nn.functional.dropout(param, p=weight_dropout))
+#      print(name, param.size())
+#
+
+   rnn_drop.train(True)
+   startTime = time.time()
+   trainChars = 0
+   for offset in range(0, len(numericAndBoundaries), args.batchSize):
+      printHere = (int(offset/args.batchSize) % 50 == 0)
+      loss, charCounts = forward(numericAndBoundaries[offset:offset+args.batchSize], printHere=printHere, train=True)
+      backward(loss, printHere)
+      trainChars += charCounts 
+      if printHere:
+          print("Dev losses")
+          print(devLosses)
+          print("Chars per sec "+str(trainChars/(time.time()-startTime)))
+
+   rnn_drop.train(False)
+#   for name, param in rnn.named_parameters():
+#      setattr(rnn, name, oldParameters[name])
+
+
+     
+   dev_loss = 0
+   dev_char_count = 0
+   dev_data = dev.iterator()
+   numericAndBoundaries = prepareDataset(dev_data)
+
+   for offset in range(0, len(numericAndBoundaries), args.batchSize):
+       printHere = (int(offset/args.batchSize) % 10 == 0)
+       loss, numberOfCharacters = forward(numericAndBoundaries[offset:offset+args.batchSize], printHere=printHere, train=False)
+       dev_loss += numberOfCharacters * loss.cpu().data.numpy()[0]
+       dev_char_count += numberOfCharacters
+   devLosses.append(dev_loss/dev_char_count)
+   print(devLosses)
+   with open("/checkpoint/mhahn/"+args.language+"_"+__file__+"_"+str(args.myID), "w") as outFile:
+      print(" ".join([str(x) for x in devLosses]), file=outFile)
+
+   if len(devLosses) > 1 and devLosses[-1] > devLosses[-2]:
+      break
+   if args.save_to is not None:
+      torch.save(dict([(name, module.state_dict()) for name, module in named_modules.items()]), "/checkpoint/mhahn/"+args.save_to+".pth.tar")
+
+
