46eb012a3b96f5d5ff78c17060327f14887597d5
georgialoukatou <georgialoukatou@gmail.com>
2018-10-23 14:51:24 +0200

    optimization files

--- lm-acqdiv.py
+++ lm-acqdiv.py
@@ -1,7 +1,6 @@
 from config import VOCAB_HOME, CHAR_VOCAB_HOME, CHECKPOINT_HOME
 
-
-
+import os
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--language", dest="language", type=str)
@@ -17,7 +16,7 @@ parser.add_argument("--layer_num", type=int, default=1)
 parser.add_argument("--weight_dropout_in", type=float, default=0.01)
 parser.add_argument("--weight_dropout_hidden", type=float, default=0.1)
 parser.add_argument("--char_dropout_prob", type=float, default=0.33)
-parser.add_argument("--char_noise_prob", type = float, default= 0.01)
+parser.add_argument("--char_noise_prob", type = float, default= 0.0)
 parser.add_argument("--learning_rate", type = float, default= 0.1)
 parser.add_argument("--myID", type=int, default=random.randint(0,1000000000))
 parser.add_argument("--sequence_length", type=int, default=50)
@@ -25,9 +24,6 @@ parser.add_argument("--sequence_length", type=int, default=50)
 
 args=parser.parse_args()
 print(args)
-
-
-
 
 
 from acqdivReader import AcqdivReader, AcqdivReaderPartition
@@ -44,21 +40,21 @@ def plus(it1, it2):
 
 try:
    with open(CHAR_VOCAB_HOME+"/char-vocab-acqdiv-"+args.language, "r") as inFile:
-     itos = inFile.read().strip().split("\n")
+      itos = inFile.read().strip().split("\n")
 except FileNotFoundError:
-    print("Creating new vocab")
-    char_counts = {}
-    # get symbol vocabulary
-    with open(VOCAB_HOME+args.language+"-vocab.txt", "r") as inFile:
+   print("Creating new vocab")
+   char_counts = {}
+   # get symbol vocabulary
+   with open(VOCAB_HOME+args.language+"-vocab.txt", "r") as inFile:
       words = inFile.read().strip().split("\n")
       for word in words:
          for char in word.lower():
             char_counts[char] = char_counts.get(char, 0) + 1
-    char_counts = [(x,y) for x, y in char_counts.items()]
-    itos = [x for x,y in sorted(char_counts, key=lambda z:(z[0],-z[1]))]
-    with open(CHAR_VOCAB_HOME+"/char-vocab-acqdiv-"+args.language, "w") as outFile:
-       print("\n".join(itos), file=outFile)
-#itos = sorted(itos)
+            char_counts = [(x,y) for x, y in char_counts.items()]
+            itos = [x for x,y in sorted(char_counts, key=lambda z:(z[0],-z[1]))]
+   with open(CHAR_VOCAB_HOME+"/char-vocab-acqdiv-"+args.language, "w") as outFile:
+      outFile.write("\n".join(itos) + '\n')
+      #itos = sorted(itos)
 itos.append("\n")
 print(itos)
 stoi = dict([(itos[i],i) for i in range(len(itos))])
@@ -91,6 +87,7 @@ char_embeddings = torch.nn.Embedding(num_embeddings=len(itos)+3, embedding_dim=a
 
 logsoftmax = torch.nn.LogSoftmax(dim=2)
 
+
 train_loss = torch.nn.NLLLoss(ignore_index=0)
 print_loss = torch.nn.NLLLoss(size_average=False, reduce=False, ignore_index=0)
 char_dropout = torch.nn.Dropout2d(p=args.char_dropout_prob)
@@ -99,7 +96,7 @@ modules = [rnn, output, char_embeddings]
 def parameters():
    for module in modules:
        for param in module.parameters():
-            yield param
+          yield param
 
 parameters_cached = [x for x in parameters()]
 
@@ -108,30 +105,30 @@ optim = torch.optim.SGD(parameters(), lr=args.learning_rate, momentum=0.0) # 0.0
 named_modules = {"rnn" : rnn, "output" : output, "char_embeddings" : char_embeddings, "optim" : optim}
 
 if args.load_from is not None:
-  checkpoint = torch.load(CHECKPOINT_HOME+args.load_from+".pth.tar")
-  for name, module in named_modules.items():
+   checkpoint = torch.load(CHECKPOINT_HOME+args.load_from+".pth.tar")
+   for name, module in named_modules.items():
       module.load_state_dict(checkpoint[name])
 
 from torch.autograd import Variable
 
 
-# ([0] + [stoi[training_data[x]]+1 for x in range(b, b+sequence_length) if x < len(training_data)]) 
+# ([0] + [stoi[training_data[x]]+1 for x in range(b, b+sequence_length) if x < len(training_data)])
 
 #from embed_regularize import embedded_dropout
 
 
 def prepareDatasetChunks(data, train=True):
-      numeric = [0]
-      count = 0
-      print("Prepare chunks")
-      for chunk in data:
-#       print(len(chunk))
-       for char in chunk:
+   numeric = [0]
+   count = 0
+   print("Prepare chunks")
+   for chunk in data:
+      #       print(len(chunk))
+      for char in chunk:
          if char == " ":
-           continue
+            continue
          count += 1
-#         if count % 100000 == 0:
-#             print(count/len(data))
+         #         if count % 100000 == 0:
+         #             print(count/len(data))
          numeric.append((stoi[char]+3 if char in stoi else 2) if (not train) or random.random() > args.char_noise_prob else 2+random.randint(0, len(itos)))
          if len(numeric) > args.sequence_length:
             yield numeric
@@ -141,68 +138,64 @@ def prepareDatasetChunks(data, train=True):
 
 
 def prepareDataset(data, train=True):
-      numeric = [0]
-      count = 0
-      for char in data:
-         if char == " ":
-           continue
-         count += 1
-#         if count % 100000 == 0:
-#             print(count/len(data))
-         numeric.append((stoi[char]+3 if char in stoi else 2) if (not train) or random.random() > args.char_noise_prob else 2+random.randint(0, len(itos)))
-         if len(numeric) > args.sequence_length:
-            yield numeric
-            numeric = [0]
+   numeric = [0]
+   count = 0
+   for char in data:
+      if char == " ":
+         continue
+      count += 1
+      #         if count % 100000 == 0:
+      #             print(count/len(data))
+      numeric.append((stoi[char]+3 if char in stoi else 2) if (not train) or random.random() > args.char_noise_prob else 2+random.randint(0, len(itos)))
+      if len(numeric) > args.sequence_length:
+         yield numeric
+         numeric = [0]
 
 
 def forward(numeric, train=True, printHere=False):
-      input_tensor = Variable(torch.LongTensor(numeric).transpose(0,1)[:-1].cuda(), requires_grad=False)
-      target_tensor = Variable(torch.LongTensor(numeric).transpose(0,1)[1:].cuda(), requires_grad=False)
-
+    input_tensor = Variable(torch.LongTensor(numeric).transpose(0,1)[:-1].cuda(), requires_grad=False)
+    target_tensor = Variable(torch.LongTensor(numeric).transpose(0,1)[1:].cuda(), requires_grad=False)
 
     #  print(char_embeddings)
-      #if train and (embedding_full_dropout_prob is not None):
-      #   embedded = embedded_dropout(char_embeddings, input_tensor, dropout=embedding_full_dropout_prob, scale=None) #char_embeddings(input_tensor)
-      #else:
-      embedded = char_embeddings(input_tensor)
-      if train:
-         embedded = char_dropout(embedded)
+    #if train and (embedding_full_dropout_prob is not None):
+    #   embedded = embedded_dropout(char_embeddings, input_tensor, dropout=embedding_full_dropout_prob, scale=None) #char_embeddings(input_tensor)
+    #else:
+    embedded = char_embeddings(input_tensor)
+    if train:
+       embedded = char_dropout(embedded)
 
-      out, _ = rnn_drop(embedded, None)
-#      if train:
-#          out = dropout(out)
+    out, _ = rnn_drop(embedded, None)
+    #      if train:
+    #          out = dropout(out)
 
-      logits = output(out) 
-      log_probs = logsoftmax(logits)
-   #   print(logits)
-  #    print(log_probs)
- #     print(target_tensor)
+    logits = output(out)
+    log_probs = logsoftmax(logits)
+    #   print(logits)
+    #    print(log_probs)
+    #     print(target_tensor)
 
-      loss = train_loss(log_probs.view(-1, len(itos)+3), target_tensor.view(-1))
+    loss = train_loss(log_probs.view(-1, len(itos)+3), target_tensor.view(-1))
 
-      if printHere:
-         lossTensor = print_loss(log_probs.view(-1, len(itos)+3), target_tensor.view(-1)).view(args.sequence_length, len(numeric))
-         losses = lossTensor.data.cpu().numpy()
-#         boundaries_index = [0 for _ in numeric]
-         for i in range((args.sequence_length-1)-1):
- #           if boundaries_index[0] < len(boundaries[0]) and i+1 == boundaries[0][boundaries_index[0]]:
-  #             boundary = True
-   #            boundaries_index[0] += 1
-    #        else:
-     #          boundary = False
-            print((losses[i][0], itos[numeric[0][i+1]-3]))
-      return loss, len(numeric) * args.sequence_length
+    if printHere:
+       lossTensor = print_loss(log_probs.view(-1, len(itos)+3), target_tensor.view(-1)).view(args.sequence_length, len(numeric))
+       losses = lossTensor.data.cpu().numpy()
+       #         boundaries_index = [0 for _ in numeric]
+       for i in range((args.sequence_length-1)-1):
+          #           if boundaries_index[0] < len(boundaries[0]) and i+1 == boundaries[0][boundaries_index[0]]:
+          #             boundary = True
+          #            boundaries_index[0] += 1
+          #        else:
+          #          boundary = False
+          print((losses[i][0], itos[numeric[0][i+1]-3]))
+    return loss, len(numeric) * args.sequence_length
 
 def backward(loss, printHere):
-      optim.zero_grad()
-      if printHere:
-         print(loss)
+   optim.zero_grad()
+   if printHere:
+      print(loss)
       loss.backward()
       torch.nn.utils.clip_grad_value_(parameters_cached, 5.0) #, norm_type="inf")
       optim.step()
-
-
-
 
 import time
 
@@ -212,8 +205,6 @@ for epoch in range(10000):
    training_data = AcqdivReaderPartition(acqdivCorpusReader, "train").reshuffledIterator()
    print("Got data")
    training_chars = prepareDatasetChunks(training_data, train=True)
-
-
 
    rnn_drop.train(True)
    startTime = time.time()
@@ -228,12 +219,12 @@ for epoch in range(10000):
       printHere = (counter % 50 == 0)
       loss, charCounts = forward(numeric, printHere=printHere, train=True)
       backward(loss, printHere)
-      trainChars += charCounts 
+      trainChars += charCounts
       if printHere:
-          print((epoch,counter))
-          print("Dev losses")
-          print(devLosses)
-          print("Chars per sec "+str(trainChars/(time.time()-startTime)))
+         print((epoch,counter))
+         print("Dev losses")
+         print(devLosses)
+         print("Chars per sec "+str(trainChars/(time.time()-startTime)))
       if counter % 20000 == 0 and epoch == 0:
         if args.save_to is not None:
            torch.save(dict([(name, module.state_dict()) for name, module in named_modules.items()]), CHECKPOINT_HOME+args.save_to+".pth.tar")
@@ -247,29 +238,43 @@ for epoch in range(10000):
    dev_chars = prepareDatasetChunks(dev_data, train=True)
 
 
-     
+
    dev_loss = 0
    dev_char_count = 0
    counter = 0
 
    while True:
-       counter += 1
-       try:
+      counter += 1
+      try:
           numeric = [next(dev_chars) for _ in range(args.batchSize)]
-       except StopIteration:
+      except StopIteration:
           break
-       printHere = (counter % 50 == 0)
-       loss, numberOfCharacters = forward(numeric, printHere=printHere, train=False)
-       dev_loss += numberOfCharacters * loss.cpu().data.numpy()
-       dev_char_count += numberOfCharacters
+      printHere = (counter % 50 == 0)
+      loss, numberOfCharacters = forward(numeric, printHere=printHere, train=False)
+      dev_loss += numberOfCharacters * loss.cpu().data.numpy()
+      dev_char_count += numberOfCharacters
    devLosses.append(dev_loss/dev_char_count)
-   print(devLosses)
-   with open(CHECKPOINT_HOME+args.language+"_"+__file__+"_"+str(args.myID), "w") as outFile:
-      print(" ".join([str(x) for x in devLosses]), file=outFile)
-
+   with open(CHECKPOINT_HOME+args.language+"_"+os.path.basename(__file__)+"_"+str(args.myID), "w") as outFile:
+      outFile.write(" ".join([str(x) for x in devLosses]) + '\n')
+   if  len(devLosses)>1 and devLosses[-2] < devLosses [-1]:
+      min_loss="minimum loss=" + str(float(devLosses[-2])) +" epoch=" + str(epoch-1) + " args=" + str(args)
+      break
+   else:
+      min_loss="minimum loss=" + str(float(devLosses[-1])) +" epoch=" + str(epoch) + " args=" + str(args)
+ 
    if len(devLosses) > 1 and devLosses[-1] > devLosses[-2]:
       break
    if args.save_to is not None:
       torch.save(dict([(name, module.state_dict()) for name, module in named_modules.items()]), CHECKPOINT_HOME+args.save_to+".pth.tar")
 
 
+print(min_loss)
+
+#with open(CHECKPOINT_HOME + "/results.txt", 'a') as f:
+ #  if len(devLosses) > 1 and devLosses[-1] > devLosses[-2]:
+  #    f.write(' '.join(( args, (epoch-1), devLosses [-2])) + '\n')
+   #else:
+    #  if len(devLosses)>1:
+     # 		f.write(' '.join(( args, epoch, devLosses [-1])) + '\n')
+
+	
