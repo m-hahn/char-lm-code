a599f81c7b16e9802d9c7417ec59f5907733024e
Michael Hahn <mhahn29@gmail.com>
2018-11-19 14:59:04 -0800

    replaced hard-coded paths

---
+++ paths.py
@@ -0,0 +1,7 @@
+CHAR_VOCAB_HOME = "vocabularies/"
+MODELS_HOME = "/checkpoint/mhahn/" 
+LOG_HOME = "/checkpoint/mhahn/" # for logging validation losses
+FIGURES_HOME = "/checkpoint/mhahn/"
+
+WIKIPEDIA_HOME = "/private/home/mhahn/data/WIKIPEDIA/"
+
