2444e1e0075cda8efac9988435d034e68a7e3691
Michael Hahn <mhahn29@gmail.com>
2019-01-03 20:54:03 -0800

    segmentation

--- char-lm-ud-wiki-classify-boundaries-report-errors-fullbalance.py
+++ char-lm-ud-wiki-classify-boundaries-report-errors-fullbalance.py
@@ -1,10 +1,3 @@
-# python char-lm-ud-wiki-classify-boundaries-report-errors-upToEndOnly-tokenized-nobalance.py --language english  --batchSize 128 --char_dropout_prob 0.001 --char_embedding_size 200 --char_noise_prob 0.0 --hidden_dim 1024 --language english --layer_num 3 --learning_rate 3.6  --myID 282506230 --load-from wiki-english-nospaces-bptt-282506230 --sequence_length 80 --weight_dropout_hidden 0.01 --weight_dropout_in 0.0
-
-# python char-lm-ud-wiki-classify-boundaries-report-errors-upToEndOnly-tokenized-nobalance.py --language german --batchSize 128 --char_embedding_size 100 --hidden_dim 1024 --layer_num 2 --weight_dropout_in 0.1 --weight_dropout_hidden 0.2 --char_dropout_prob 0.0 --char_noise_prob 0.01 --learning_rate 0.2 --load-from wiki-german-nospaces-bptt-910515909
-
-# python char-lm-ud-wiki-classify-boundaries-report-errors-upToEndOnly-tokenized-nobalance.py --language italian --batchSize 128 --char_embedding_size 200 --hidden_dim 1024 --layer_num 2 --weight_dropout_in 0.1 --weight_dropout_hidden 0.2 --char_dropout_prob 0.0 --char_noise_prob 0.01 --learning_rate 0.2 --load-from wiki-italian-nospaces-bptt-855947412
-
-
 from paths import WIKIPEDIA_HOME
 from paths import LOG_HOME
 from paths import CHAR_VOCAB_HOME
@@ -16,7 +9,6 @@ parser.add_argument("--language", dest="language", type=str)
 parser.add_argument("--load-from", dest="load_from", type=str)
 
 import random
-random.seed(1) # May nonetheless not be reproducible, since the classifier library doesn't seem to allow setting the seed
 
 parser.add_argument("--batchSize", type=int, default=16)
 parser.add_argument("--char_embedding_size", type=int, default=100)
@@ -38,7 +30,7 @@ print(args)
 #assert args.language == "german"
 
 
-import corpusIteratorWikiWords
+import corpusIteratorWiki
 
 
 
@@ -135,29 +127,47 @@ def prepareDatasetChunks(data, train=True):
       currentWord = ""
       print("Prepare chunks")
       for chunk in data:
-          print(len(chunk))
-          for word in chunk:
-#             assert word != "popula"
-             for char in word:
-                if boundariesAll[len(numeric)] is None:
-                      boundariesAll[len(numeric)] = currentWord
-       
-                count += 1
-                currentWord += char
-                numeric.append((stoi[char]+3 if char in stoi else 2) if (not train) or random.random() > args.char_noise_prob else 2+random.randint(0, len(itos)))
-                if len(numeric) > args.sequence_length:
-                   yield numeric, boundaries, boundariesAll
-                   numeric = [0]
-                   boundaries = [None for _ in range(args.sequence_length+1)]
-                   boundariesAll = [None for _ in range(args.sequence_length+1)]
-      
-             assert currentWord == word, (currentWord, word)
-             boundaries[len(numeric)] = currentWord
-             boundariesAll[len(numeric)] = currentWord
-       
-             currentWord = ""
-       
-  
+       print(len(chunk))
+       for char in chunk:
+         if char == " ":
+           boundaries[len(numeric)] = currentWord
+           boundariesAll[len(numeric)] = currentWord
+
+           currentWord = ""
+           continue
+         else:
+           if boundariesAll[len(numeric)] is None:
+               boundariesAll[len(numeric)] = currentWord
+
+         count += 1
+         currentWord += char
+#         if count % 100000 == 0:
+#             print(count/len(data))
+         numeric.append((stoi[char]+3 if char in stoi else 2) if (not train) or random.random() > args.char_noise_prob else 2+random.randint(0, len(itos)))
+         if len(numeric) > args.sequence_length:
+            yield numeric, boundaries, boundariesAll
+            numeric = [0]
+            boundaries = [None for _ in range(args.sequence_length+1)]
+            boundariesAll = [None for _ in range(args.sequence_length+1)]
+
+
+
+
+
+#def prepareDataset(data, train=True):
+#      numeric = [0]
+#      count = 0
+#      for char in data:
+#         if char == " ":
+#           continue
+#         count += 1
+##         if count % 100000 == 0:
+##             print(count/len(data))
+#         numeric.append((stoi[char]+3 if char in stoi else 2) if (not train) or random.random() > args.char_noise_prob else 2+random.randint(0, len(itos)))
+#         if len(numeric) > args.sequence_length:
+#            yield numeric
+#            numeric = [0]
+#
 
 # from each bath element, get one positive example OR one negative example
 
@@ -168,9 +178,11 @@ relevantWords = []
 relevantNextWords = []
 labels_sum = 0
 
-def forward(numeric, train=True, printHere=False, enforceBalancing=True):
+def forward(numeric, train=True, printHere=False):
       global labels_sum
       numeric, boundaries, boundariesAll = zip(*numeric)
+#      print(numeric)
+ #     print(boundaries)
 
       input_tensor = Variable(torch.LongTensor(numeric).transpose(0,1)[:-1].cuda(), requires_grad=False)
       target_tensor = Variable(torch.LongTensor(numeric).transpose(0,1)[1:].cuda(), requires_grad=False)
@@ -180,27 +192,32 @@ def forward(numeric, train=True, printHere=False, enforceBalancing=True):
          embedded = char_dropout(embedded)
 
       out, _ = rnn_drop(embedded, None)
+#      if train:
+#          out = dropout(out)
 
       for i in range(len(boundaries)): # for each batch sample
          target = (labels_sum + 10 < len(labels)/2) or (random.random() < 0.5) # decide whether to get positive or negative sample
-         true = sum([((x == None) if target == False else (x is not None and x not in wordsSoFar)) for x in boundaries[i][int(args.sequence_length/2):-10]]) # candidates
+         true = sum([((x == None) if target == False else (y not in wordsSoFar)) for x, y in list(zip(boundaries[i], boundariesAll[i]))[int(args.sequence_length/2):-1]]) # condidates
+ #        print(target, true)
          if true == 0:
             continue
          soFar = 0
-         for j in range(int(len(boundaries[i])/2), len(boundaries[i])-10):
-           if (lambda x:((x is None if target == False else (x is not None and x not in wordsSoFar))))(boundaries[i][j]):
+#         print(list(zip(boundaries[i], boundariesAll[i])))
+         for j in range(len(boundaries[i])-5):
+           if j < int(len(boundaries[i])/2):
+               continue
+           if (lambda x, y:((x is None if target == False else y not in wordsSoFar)))(boundaries[i][j], boundariesAll[i][j]):
+ #             print(i, target, true,soFar)
               if random.random() < 1/(true-soFar):
-                  assert (boundaries[i][j] is not None) == target, (boundaries[i][j], boundariesAll[i][j], target)
-                  hidden_states.append(out[j-1,i].detach().data.cpu().numpy())
+                  hidden_states.append(out[j,i].detach().data.cpu().numpy())
                   labels.append(1 if target else 0)
                   relevantWords.append(boundariesAll[i][j])
                   relevantNextWords.append(([boundaries[i][k] for k in range(j+1, len(boundaries[i])) if boundaries[i][k] is not None]+["END_OF_SEQUENCE"])[0])
-#                  print(target, relevantWords[-1], relevantNextWords[-1])
                   assert boundariesAll[i][j] is not None
 
                   labels_sum += labels[-1]
-                  if target and enforceBalancing:
-                     wordsSoFar.add(boundaries[i][j])
+                  if target:
+                     wordsSoFar.add(boundariesAll[i][j])
                   break
               soFar += 1
          assert soFar < true
@@ -236,7 +253,7 @@ import time
 devLosses = []
 #for epoch in range(10000):
 if True:
-   training_data = corpusIteratorWikiWords.dev(args.language)
+   training_data = corpusIteratorWiki.training(args.language)
    print("Got data")
    training_chars = prepareDatasetChunks(training_data, train=True)
 
@@ -261,14 +278,14 @@ if True:
           print(devLosses)
           print("Chars per sec "+str(trainChars/(time.time()-startTime)))
 
-      if len(labels) > 1000:
+      if len(labels) > 10000:
          break
 
 predictors = hidden_states
 dependent = labels
 
 
-TEST_FRACTION = 0.9
+TEST_FRACTION = 0.5
 
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test, words_train, words_test, next_words_train, next_words_test = train_test_split(predictors, dependent, relevantWords, relevantNextWords, test_size=TEST_FRACTION, random_state=random.randint(1,100), shuffle=True)
@@ -282,99 +299,56 @@ logisticRegr = LogisticRegression()
 
 logisticRegr.fit(x_train, y_train)
 
+predictions = logisticRegr.predict(x_test)
+
+
+score = logisticRegr.score(x_test, y_test)
 
 errors = []
-scores = []
-
-for _ in range(30):
-
-     wordsSoFar = set()
-     hidden_states = []
-     labels = []
-     relevantWords = []
-     relevantNextWords = []
-     labels_sum = 0
-     
-     
-     devLosses = []
-     #for epoch in range(10000):
-     if True:
-     
-     
-        rnn_drop.train(False)
-        startTime = time.time()
-        trainChars = 0
-        counter = 0
-        while True:
-           counter += 1
-           try:
-              numeric = [next(training_chars) for _ in range(args.batchSize)]
-           except StopIteration:
-              break
-           printHere = (counter % 50 == 0)
-           forward(numeric, printHere=printHere, train=True, enforceBalancing=False)
-           #backward(loss, printHere)
-           if printHere:
-               print((counter))
-               print("Dev losses")
-               print(devLosses)
-               print("Chars per sec "+str(trainChars/(time.time()-startTime)))
-     
-           if len(labels) > 10000:
-              break
-     if len(hidden_states) == 0:
-          break
-     predictors = hidden_states
-     dependent = labels
-     
-     x_test = predictors
-     y_test = dependent
-     words_test = relevantWords
-     next_words_test = relevantNextWords
-     
-   #  for i in range(len(x_test)):
-    #     print(y_test[i], words_test[i], next_words_test[i])
-     
-     
-     
-     predictions = logisticRegr.predict(x_test)
-     
-     
-     score = logisticRegr.score(x_test, y_test)
-     scores.append(score)
-
-     for i in range(len(predictions)):
-         if predictions[i] != y_test[i]:
-               errors.append((y_test[i], (words_test[i], next_words_test[i], predictions[i], y_test[i])))
-     print("Balance ",sum(y_test)/len(y_test))
-
-falsePositives = {}
-falseNegatives = {}
+for i in range(len(predictions)):
+    if predictions[i] != y_test[i]:
+          errors.append((y_test[i], (words_test[i], next_words_test[i], predictions[i], y_test[i])))
 for error in errors:
    if error[0] == 0:
-      record = error[1][0]+"|"+error[1][1]
-      falsePositives[record] = falsePositives.get(record, 0)+1
-   elif error[0] == 1:
-      record = error[1][0]+" "+error[1][1]
-      falseNegatives[record] = falseNegatives.get(record, 0)+1
+      print(error[1][0]+"|"+error[1][1])
+for error in errors:
+   if error[0] == 1:
+      print(error[1][0]+" "+error[1][1])
 
-falsePositives = sorted(list(falsePositives.items()), key=lambda x:x[1])
-falseNegatives = sorted(list(falseNegatives.items()), key=lambda x:x[1])
-
-with open(f"results/segmentation-{args.language}-frequent-errors.txt", "w") as outFile:
-   print("False Positives", file=outFile)
-   for error in falsePositives[-30:]:
-      print(error[0]+"\t"+str(error[1]), file=outFile)
-
-   print("", file=outFile)   
-   print("False Negatives", file=outFile)
-   for error in falseNegatives[-30:]:
-      print(error[0]+"\t"+str(error[1]), file=outFile)
 
 
 print(len(predictions))
 
-score = sum(scores)/len(scores)
+print("Balance ",sum(y_test)/len(y_test))
 print(score)
 
+
+#   dev_data = corpusIteratorWiki.dev(args.language)
+#   print("Got data")
+#   dev_chars = prepareDataset(dev_data, train=True) if args.language == "italian" else prepareDatasetChunks(dev_data, train=True)
+#
+#
+#     
+#   dev_loss = 0
+#   dev_char_count = 0
+#   counter = 0
+#
+#   while True:
+#       counter += 1
+#       try:
+#          numeric = [next(dev_chars) for _ in range(args.batchSize)]
+#       except StopIteration:
+#          break
+#       printHere = (counter % 50 == 0)
+#       loss, numberOfCharacters = forward(numeric, printHere=printHere, train=False)
+#       dev_loss += numberOfCharacters * loss.cpu().data.numpy()[0]
+#       dev_char_count += numberOfCharacters
+#   devLosses.append(dev_loss/dev_char_count)
+#   print(devLosses)
+#   with open(LOG_HOME+"/"+args.language+"_"+__file__+"_"+str(args.myID), "w") as outFile:
+#      print(" ".join([str(x) for x in devLosses]), file=outFile)
+#
+#   if len(devLosses) > 1 and devLosses[-1] > devLosses[-2]:
+#      break
+#
 
